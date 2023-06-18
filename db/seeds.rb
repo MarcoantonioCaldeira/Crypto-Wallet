0d9203b62d3677f5dbe50f583420550a2db501b6
Marco Antonio Caldeira <caldeiran391@gmail.com>
2023-06-18 20:05:08 -0300

    Add new coin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,23 @@
-Coin.create!(
-  description: 'Bitcoin',
-  acronym:"BTC",
-  url_image: "https://w7.pngwing.com/pngs/450/133/png-transparent-bitcoin-cryptocurrency-virtual-currency-decal-blockchain-info-bitcoin-text-trademark-logo.png"
-)
 
-Coin.create!(
-  description:"Ethereum",
-  acronym: 'ETH',
-  url_image: 'https://w7.pngwing.com/pngs/368/176/png-transparent-ethereum-cryptocurrency-blockchain-bitcoin-logo-bitcoin-angle-triangle-logo-thumbnail.png'
-)
+puts "Cadastro de moedas..."
 
-Coin.create(
-  description: "Dosh",
-  acronym: 'ETH',
-  url_image: 'https://cdn.freebiesupply.com/logos/large/2x/dash-3-logo-png-transparent.png'
-)
+coins = [
+    { description: 'Bitcoin',
+      acronym:"BTC",
+      url_image: "https://w7.pngwing.com/pngs/450/133/png-transparent-bitcoin-cryptocurrency-virtual-currency-decal-blockchain-info-bitcoin-text-trademark-logo.png" },
+
+    { description:"Ethereum",
+      acronym: 'ETH',
+      url_image: 'https://w7.pngwing.com/pngs/368/176/png-transparent-ethereum-cryptocurrency-blockchain-bitcoin-logo-bitcoin-angle-triangle-logo-thumbnail.png' },
+
+    { description: "Dosh",
+      acronym: 'ETH',
+      url_image: 'https://cdn.freebiesupply.com/logos/large/2x/dash-3-logo-png-transparent.png'}
+  ]
+
+coins.each do |coin|
+  Coin.find_or_create_by!(coin)
+end
+
+puts "Moedas cadastradas com sucesso"
 
