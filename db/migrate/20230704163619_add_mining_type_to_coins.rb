36d58d13a6658f57c860fb4e85991ac6fb985da9
Marco Antonio Caldeira <caldeiran391@gmail.com>
2023-07-20 14:38:28 -0300

    resolving errors

--- db/migrate/20230704163619_add_mining_type_to_coins.rb
+++ db/migrate/20230704163619_add_mining_type_to_coins.rb
@@ -1,5 +1,5 @@
 class AddMiningTypeToCoins < ActiveRecord::Migration[7.0]
   def change
-    add_reference :coins, :mining_types, null: false, foreign_key: true
+    add_reference :coins, :mining_type, foreign_key: true
   end
 end
