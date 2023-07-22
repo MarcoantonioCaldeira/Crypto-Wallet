34ff79b0eb5c9b64543fe3801b8d52811b83344a
Marco Antonio Caldeira <caldeiran391@gmail.com>
2023-07-22 18:25:50 -0300

    add new functions

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,8 @@ Rails.application.config.assets.version = "1.0"
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
+
+Rails.application.config.assets.precompile += %w( application.js welcome.js )
+
+Rails.application.config.assets.precompile += %w( application.css scaffolds.css welcome.css )
